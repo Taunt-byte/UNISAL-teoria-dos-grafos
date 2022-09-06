630a119785937fda514287a00970c6ec5895a849
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-09-06 19:41:21 -0300

    Create Lista-1-Atividade16.py

---
+++ Lista-1-Atividade16.py
@@ -0,0 +1,17 @@
+grafo = {
+    #vertices
+    "Pouso Alegre":   {"jacutinga": 15, "Ouro-Fino": 20, "Borda-da-mata": 30},
+    "Pouso Alegre":     {"jacutinga": 10, "Cachoeira": 20, "Ouro-Fino": 25, "Piquete": 8},
+    "Pouso Alegre":    {"jacutinga": 8, "Delfim Moreira": 15},
+    "Pouso Alegre":  {"jacutinga": 5, "estiva": 25, "Bom-repouso": 12},
+    "Pouso Alegre":    {"jacutinga": 30, "monte-verde": 40},
+    "Pouso Alegre":    {"jacutinga": 40, "Ouro-Fino": 20}
+}
+
+print(grafo)
+for i in grafo:
+    print(i)
+
+# Grafo não esta gerando corretamente
+# Arrumar Vertices 
+# Arrumar Arestas
